cbb7a10f9856e7dee85147339d89b0f08ab01cac
Glenn Torrens <glenntorrens@gmail.com>
2026-04-17 17:57:50 +0900

    implemented fonts to tailwindconfig etc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
-
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import "./globals.css";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded){
+     SplashScreen.hideAsync(); 
+    }
+  }, [fontsLoaded]);
+  
+  if(!fontsLoaded) return null;
+
   return <Stack />;
 }
