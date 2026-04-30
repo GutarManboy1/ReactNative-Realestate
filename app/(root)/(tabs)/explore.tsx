8ffc12b8d2e6c9b180d5708eea318cfdb3e814b0
Glenn Torrens <glenntorrens@gmail.com>
2026-04-30 18:26:49 +0900

    started implementing explore tab

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Explore = () => {
   return (
-    <View>
-      <Text>explore</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView>explore</ScrollView>
+    </SafeAreaView>
   )
 }
 
